796d3af1088dfb907bc7259c76d7266f4fa63c79
mendieta967 <olorteguopedro@gmail.com>
2024-08-28 14:28:52 -0300

    Ej6ResueltoLinq

---
+++ src/Web/Controllers/LinqEj6Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj6Controller : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Get([FromQuery] List<string> lista)
+        { 
+            var query = from word in lista
+                        select word.Replace("ia", "*");
+
+            return query.ToList();
+        }
+    }
+}

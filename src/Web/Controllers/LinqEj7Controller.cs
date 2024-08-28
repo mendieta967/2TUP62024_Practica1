242aa3af08654d4d6c795c75130ab63daee3a5f0
mendieta967 <olorteguopedro@gmail.com>
2024-08-28 14:54:27 -0300

    Ej7resueltoLinq

---
+++ src/Web/Controllers/LinqEj7Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj7Controller : ControllerBase
+    {
+        [HttpGet]
+
+        public IList<string> Get([FromQuery] string words)
+        {
+            return DevolverPalabrasCapitales(words);
+        }
+        public static IList<string> DevolverPalabrasCapitales(string cadena)
+        {
+            var splitChain = cadena.Split(" ");
+
+            var query = from word in splitChain
+                        where word == (word.ToUpper())
+                        select word;
+
+            return query.ToList();
+
+        }
+    }
+}
+
+//Implementar la función public static IList<string> DevolverPalabrasCapitales(string cadena)
+//que devuelva sólo las palabras en mayúsculas de la cadena.
+//Ejemplo: "ESTO es un EJEMPLO del EJERcicio" → ESTO, EJEMPLO

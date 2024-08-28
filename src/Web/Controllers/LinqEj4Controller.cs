cd693a453fc7c6eb6ae003022f643fc5fc1bf17e
mendieta967 <olorteguopedro@gmail.com>
2024-08-28 13:57:34 -0300

    Ej4LinqResuelto

---
+++ src/Web/Controllers/LinqEj4Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj4Controller : ControllerBase
+    {
+ 
+
+
+    }
+}

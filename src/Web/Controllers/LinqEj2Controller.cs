a1495d2b6ea405a51af0503d0d4f61890cb8ba0e
mendieta967 <olorteguopedro@gmail.com>
2024-08-28 13:46:08 -0300

    Ejercicio2linqresuelto

---
+++ src/Web/Controllers/LinqEj2Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public string[] Get([FromQuery] List<string> words)
+        {
+            var query =
+                from w in words
+                where w.Length > 4
+                select w.ToUpper();
+
+            return query.ToArray();
+        }
+    }
+}
+
+/*
+ Escriba una consulta que devuelva palabras de al menos 5 caracteres y las ponga en mayúsculas. 
+ Ejemplo: "computadora", "usb" → "COMPUTADORA"
+*/

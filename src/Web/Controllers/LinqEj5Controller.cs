5fd441ac3876c2ccd99a3cc85f6435845f51c0cd
mendieta967 <olorteguopedro@gmail.com>
2024-08-28 14:19:03 -0300

    Ejresueltolinq5

---
+++ src/Web/Controllers/LinqEj5Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj5Controller : ControllerBase
+    {
+        [HttpGet]
+
+        public List<string> Get([FromQuery] List<int> lista)
+        {
+            var query = from int l in lista
+                        let cuadrado = l * l
+                        where cuadrado > 20
+
+                        select $"{l} - {cuadrado}";
+
+              return query.ToList();
+        }
+    }
+}
